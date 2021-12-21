65ef411b3c26967f195beacc19caec21a1290e71
biud436 <biud436@gmail.com>
2021-12-21 14:24:52 +0900

    added a new command

--- src/commander.ts
+++ src/commander.ts
@@ -1,4 +1,7 @@
 export const Commander = {
   getLogFileCommand: (container_name: string) =>
     `docker inspect ${container_name} | grep LogPath`,
+  getDockerContainerList: () => {
+    `docker container ps --format "{{.Names}}"`;
+  },
 };
